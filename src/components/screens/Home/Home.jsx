e04ee7dde6f436116010fc32c15f44f597f47f8e
AleksandrKompanetc <moneymacker7@gmail.com>
2024-08-15 20:51:35 +0300

    added main files

---
+++ src/components/screens/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import TodoItem from './item/TodoItem';
+
+const todos = [
+  {
+    _id: 1,
+    title: 'Finish the essay collaboration',
+    isCompleted: false,
+  },
+  {
+    _id: 2,
+    title: 'Read next chapter of the book',
+    isCompleted: false,
+  },
+  {
+    _id: 3,
+    title: 'Send the finished assignment',
+    isCompleted: false,
+  }
+]
+
+const Home = () => {
+  return (
+    <div className='text-white w-4/5 mx-auto'>
+      <h1 className='text-2xl font-bold text-center mb-10'>Todo List</h1>
+      {todos.map((todo) => 
+        <TodoItem key={todo._id} todo={todo} />
+      )}
+    </div>
+  )
+}
+
+export default Home;
